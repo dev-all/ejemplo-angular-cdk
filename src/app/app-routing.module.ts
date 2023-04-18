fa969edefdb1a4fb83c22874c4d3a382802a22bf
all-developer <all.leonardo.lucero@gmail.com>
2023-04-18 19:18:09 -0300

    init +

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AccordionComponent } from './accordion/accordion.component';
+import { CollectionsComponent } from './collections/collections.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ObserverComponent } from './observer/observer.component';
+import { OverlayComponent } from './overlay/overlay.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'layout', pathMatch: 'full' },
+  { path: 'accordion', component: AccordionComponent },
+  { path: 'collections', component: CollectionsComponent },
+  { path: 'layout', component: LayoutComponent },
+  { path: 'observer', component: ObserverComponent },
+  { path: 'overlay', component: OverlayComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
